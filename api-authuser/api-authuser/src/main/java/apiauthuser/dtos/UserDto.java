d4af7cb8a30ce8fe284e65ca864db29f887211cc
Sérgio Eduardo <sergio.eng.dev@hotmail.com>
2022-09-02 22:53:30 -0300

    api-authuser
    Validação

--- api-authuser/api-authuser/src/main/java/apiauthuser/dtos/UserDto.java
+++ api-authuser/api-authuser/src/main/java/apiauthuser/dtos/UserDto.java
@@ -1,5 +1,9 @@
 package apiauthuser.dtos;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonView;
 
@@ -16,16 +20,24 @@ public class UserDto {
         public static interface ImagePut {}
     }
 	
+	@NotBlank (groups = UserView.RegistrationPost.class)
+    @Size(min = 4, max = 50, groups = UserView.RegistrationPost.class)
 	@JsonView(UserView.RegistrationPost.class)
 	private String username;
 	
+	@NotBlank (groups = UserView.RegistrationPost.class)
+	@Email(groups = UserView.RegistrationPost.class)
 	@JsonView(UserView.RegistrationPost.class)
 	private String email;
 	
+	@NotBlank(groups = {UserView.RegistrationPost.class, UserView.PasswordPut.class})
+	@Size(min = 6, max = 20, groups = {UserView.RegistrationPost.class, UserView.PasswordPut.class})
 	@JsonView({UserView.RegistrationPost.class, UserView.PasswordPut.class})
 	private String password;
 	
-	 @JsonView({UserView.PasswordPut.class})
+	@NotBlank(groups = UserView.PasswordPut.class)
+	@Size(min = 6, max = 20, groups = UserView.PasswordPut.class)
+	@JsonView({UserView.PasswordPut.class})
 	private String oldPassword;
 	
 	@JsonView({UserView.RegistrationPost.class, UserView.UserPut.class})
@@ -37,6 +49,7 @@ public class UserDto {
 	@JsonView({UserView.RegistrationPost.class, UserView.UserPut.class})
 	private String cpf;
 	
+	@NotBlank(groups = UserView.ImagePut.class)
 	@JsonView({UserView.ImagePut.class})
 	private String imageUrl;
 
