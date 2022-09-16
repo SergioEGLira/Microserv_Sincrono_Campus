adf1170a8aadccfe0f75ba7caaaadb1f2ae6f1b0
Sérgio Eduardo <sergio.eng.dev@hotmail.com>
2022-09-16 10:49:49 -0300

    api-authuser
    *Test Postman
    *Adicionado novo @Spec
    *getAllUsersPaged recebe o parâmetro de pesquisa opcional 'courseId'

--- api-authuser/api-authuser/src/main/java/apiauthuser/controllers/UserController.java
+++ api-authuser/api-authuser/src/main/java/apiauthuser/controllers/UserController.java
@@ -23,6 +23,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.annotation.JsonView;
@@ -46,8 +47,14 @@ public class UserController {
 	public ResponseEntity<Page<UserModel>> getAllUsersPaged(SpecificationTemplate.UserSpec spec,
 						@PageableDefault(page = 0, size = 20, 
 										sort = "userId", direction = Sort.Direction.ASC) 
-										Pageable pageable){
-		Page<UserModel> userModelPage = userService.findAll(spec, pageable);
+										Pageable pageable,
+										@RequestParam(required = false) UUID courseId){
+		Page<UserModel> userModelPage = null;
+		if(courseId != null) {
+			userModelPage = userService.findAll(SpecificationTemplate.userCourseId(courseId).and(spec), pageable);
+        } else {
+            userModelPage = userService.findAll(spec, pageable);
+        }
 		if(!userModelPage.isEmpty()){
             for(UserModel user : userModelPage.toList()){
                 user.add(linkTo(methodOn(UserController.class).getOneUser(user.getUserId())).withSelfRel());
