fb1ec952bf33287155648605b1bb4e871dc283fc
Sérgio Eduardo <sergio.eng.dev@hotmail.com>
2022-09-23 10:08:15 -0300

    Eureka Clients
    * Configurações Euraka Client

--- api-course/api-course/src/main/java/apicourse/ApiCourseApplication.java
+++ api-course/api-course/src/main/java/apicourse/ApiCourseApplication.java
@@ -2,8 +2,10 @@ package apicourse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ApiCourseApplication {
 
 	public static void main(String[] args) {
